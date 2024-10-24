c5c86fed4e2f524e5e119df1c9141c9ae50298c2
afkyoung <lcyongraghv@gmail.com>
2024-10-23 21:52:31 -0300

    intidal debloat Aosp
    Change-Id: Ibe43ad750d7d2b159403db71215d561f4654f4d1

---
+++ Debloat.sh
@@ -0,0 +1,10 @@
+adb shell pm uninstall -k --user 0 com.google.android.apps.turbo
+adb shell pm uninstall -k --user 0 com.android.hotwordenrollment.xgoogle
+adb shell pm uninstall -k --user 0 com.google.android.apps.wellbeing
+adb shell pm uninstall -k --user 0 com.google.android.tts
+adb shell pm uninstall -k --user 0 com.google.android.projection.gearhead
+adb shell pm uninstall -k --user 0 com.android.hotwordenrollment.okgoogle
+adb shell pm uninstall -k --user 0 com.android.bips
+adb shell pm uninstall -k --user 0 com.google.android.apps.safetyhub
+adb shell pm uninstall -k --user 0 com.android.bookmarkprovider
+adb shell pm uninstall -k --user 0 com.android.wallpaper.livepicker
