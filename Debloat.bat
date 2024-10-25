20fec937e15f2210ec210792c4dda525abafa7dc
afkyoung <lcyongraghv@gmail.com>
2024-10-25 20:20:14 -0300

    scripts:added automation
    * adb devices checks connected devices
    * adb reboot will restart the device after finishing the debloat
    
    Change-Id: Iffdd891ca890808f1d950db3de2d475a79e00aea
    Signed-off-by: afkyoung <lcyongraghv@gmail.com>

--- Debloat.bat
+++ Debloat.bat
@@ -1,4 +1,5 @@
 @echo off
+adb devices
 echo "Starting to clean your system :)"...
 adb shell pm uninstall -k --user 0 com.google.android.apps.turbo
 adb shell pm uninstall -k --user 0 com.android.hotwordenrollment.xgoogle
@@ -10,6 +11,7 @@ adb shell pm uninstall -k --user 0 com.android.bips
 adb shell pm uninstall -k --user 0 com.google.android.apps.safetyhub
 adb shell pm uninstall -k --user 0 com.android.bookmarkprovider
 adb shell pm uninstall -k --user 0 com.android.wallpaper.livepicker
+adb reboot
 echo "Clean system BY @Yongraghv"
 pause
 
