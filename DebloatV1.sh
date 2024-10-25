3767512009aa1b152cc4cdb1e4cc0d65eba25620
afkyoung <lcyongraghv@gmail.com>
2024-10-25 20:17:21 -0300

    scripts: added echo and customizations
    Change-Id: I49011c60994a6935ac464c432a7e4a0b6df896ec
    Signed-off-by: afkyoung <lcyongraghv@gmail.com>

--- DebloatV1.sh
+++ DebloatV1.sh
@@ -1,3 +1,5 @@
+@echo off
+echo "Starting to clean your system :)"...
 adb shell pm uninstall -k --user 0 com.google.android.apps.turbo
 adb shell pm uninstall -k --user 0 com.android.hotwordenrollment.xgoogle
 adb shell pm uninstall -k --user 0 com.google.android.apps.wellbeing
@@ -8,3 +10,5 @@ adb shell pm uninstall -k --user 0 com.android.bips
 adb shell pm uninstall -k --user 0 com.google.android.apps.safetyhub
 adb shell pm uninstall -k --user 0 com.android.bookmarkprovider
 adb shell pm uninstall -k --user 0 com.android.wallpaper.livepicker
+echo "Clean system BY @Yongraghv"
+pause
